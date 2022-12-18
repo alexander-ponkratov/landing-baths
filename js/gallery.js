ad5a2bcdf0ef3820908b9b9bb894f273e96793c1
alexander-ponkratov <alexander.ponkratov.n@gmail.com>
2022-12-18 09:29:40 +0700

    edit gallery.css 3

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,4 @@
+//при загрузке показать с первого фото
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -12,19 +13,35 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
+  //счетчик в цикле для скрытия всех фото/точек
   let i;
+
+  //HTMLCollection для фото/точек
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+
+  //если счетчик выходит за границы, показать с первого фото
+  //зависит от количества одновременно отображаемых фото в галерее
+  //например для 3 элементов - (n > slides.length - 2)
+  if (n > slides.length - 2) { slideIndex = 1 }
+
+  //если счетчик меньше первого эл-та, показать с последнего фото
+  //зависит от количества одновременно отображаемых фото в галерее
+  //например для 3 элементов - (slideIndex = slides.length - 2)
+  if (n < 1) { slideIndex = slides.length - 2 }
+
+  //скрыть все изображения
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  //скрыть все точки
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  //отобразить [n; n+2] фото
+  slides[slideIndex - 1].style.display = "block";
   slides[slideIndex].style.display = "block";
-  slides[slideIndex+1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex + 1].style.display = "block";
+  //отобразить точку для n эл-та
+  dots[slideIndex - 1].className += " active";
 }
